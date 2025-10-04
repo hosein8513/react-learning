fd7473fee0e40f78769912eec5af14bba2ef77b8
hosein <kazmyh279@gmail.com>
2025-10-04 10:02:22 +0330

    useRef-hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
-import { useEffect, useState } from "react"
+import { useRef, useState } from "react"
 
 const App = () => {
   const [count, setcount] = useState(0)
-  const [notif, setnotif] = useState(null)
-
-
-  useEffect(() => {
-    if (count % 10 == 0 && count !== 0) {
-      setnotif(`you riched to ${count}`)
-      setTimeout(() => {
-        setnotif(null)
-      }, 3000);
-    }
-  })
-
+  let num = useRef(0)
+  const handlein = () => {
+    num.current += 2
+    setcount(num.current +1)
+  }
   return (
-    <div className="flex justify-center items-center">
-      <div className="w-[300px] h-[400px] border rounded-lg flex flex-col gap-4 items-center">
-        <h1>smart counter</h1>
-        <div className="w-[80%] h-[40%] flex justify-center items-center relative">
-          <span className="text-[30px] font-bold">
-            {count}
-          </span>
-         {count !==0 && (
-           <div className="absolute w-[40px] h-[20px] bg-blue-400 text-white rounded-full top-[20px] right-[40px] text-center">
-            {count %2 ===0 ?"zoj":"fard"}
-           </div>
-         )}
-        </div>
-        <div className="w-[90%] flex h-[30%]">
-          <button className="w-[45%] h-full " onClick={()=>setcount(count+1)}>+</button>
-          <button className="w-[45%] h-full " onClick={()=>setcount(count-1)}>-</button>
-        </div>
-        <button className="w-[60px] h-[30px]" onClick={()=>setcount(0)}>reset</button>
-      </div>
-
-     {notif &&(
-       <div className="w-[200px] h-[100px] fixed bottom-[30px] right-[40px] rounded-lg p-4 bg-blue-400 text-white transition-all transform animate-bounce">
-        {notif}
-       </div>
-     )}
-    </div>
-
+ <div className="w-[100dvw] h-[100dvh] bg-gradient-to-r from-pink-400 to-blue-400 flex justify-center items-center">
+  <div className="w-[40%] h-[60%] border rounded-lg flex flex-col gap-4 justify-center items-center">
+    <span className="w-[50%] h-[30%] rounded-lg text-center text-[25px] border">{count}</span>
+    <button className="w-1/2 h-3/12 bg-red-500 rounded-xl" onClick={handlein}>increase</button>
+  </div>
+ </div>
   )
 
 }
