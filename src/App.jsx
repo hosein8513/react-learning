1ed5dc89806aa8862f508960e6e7e98fcac68bdb
hosein <kazmyh279@gmail.com>
2025-10-20 18:02:11 +0330

    use-navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Users from "./Users";
 import Posts from "./Posts";
-import { BrowserRouter, NavLink, Route, Routes } from "react-router";
+import { BrowserRouter, NavLink, Route, Routes, useNavigate } from "react-router";
 import Adduser from "./adduser";
 import Userdetails from "./Userdetails";
 
 const App = () => {
+const navigate = useNavigate()
+
+const handlenavigate = ()=>{
+  alert("STARTING...")
+  navigate("/users")
+}
+
   return (
     <div className="min-w-screen flex justify-center items-center p-4 gap-3">
       <div className="w-[70%] rounded-lg border bg-orange-200 flex flex-col p-4 gap-4 justify-center items-center">
@@ -14,7 +21,8 @@ const App = () => {
           <NavLink to={"/posts"} className={({ isActive }) => isActive ? "bg-indigo-600 text-white w-[15%] h-[40px] rounded-lg border-0 text-center" : "bg-gray-200 w-[15%] h-[40px] rounded-lg border-0 text-center"}>POSTS</NavLink>
         </div>
         <div className="p-4">
-          <Routes>
+          <button className="w-[80px] h-[40px] rounded-lg text-white m-4 bg-green-300" onClick={handlenavigate}>START</button>
+          <Routes> 
             <Route path="/users" element={<Users />}>
             <Route path="adduser" element={<Adduser/>}/>
             <Route path="user-details/:userId" element={<Userdetails/>}/>
