773bb8a92b026d5bfbd87d1f97219283455445e3
hosein <kazmyh279@gmail.com>
2025-10-23 10:12:02 +0330

    use-callback

--- src/Normalcomp.jsx
+++ src/Normalcomp.jsx
@@ -1,22 +1,17 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 const Normalcomp = () => {
     const [count, setcount] = useState(0)
     const [text, settext] = useState(" ")
 
-    const heavy = useMemo(() => {
-        let total = 0
-        for (let i = 0; i < 1e8; i++) {
-            total += i
-        }
-        console.log("heavy proccess started...");
-        return total + count
-
-    }, [count])
+   const todo =useCallback(()=>{
+console.log("number changed");
+setcount((prev)=>prev+1)
+   },[])
     return (
         <div className='flex flex-col p-4 gap-4'>
-           <span className='text-[20px] font-semibold text-blue-300'>{heavy}</span>
-           <button className='w-[90px] h-[50px] bg-purple-400 text-white font-semibold rounded-lg' onClick={()=>{setcount((prev)=>prev +1)}}>COUNT</button>
+           <span className='text-[20px] font-semibold text-blue-300'>{count}</span>
+           <button className='w-[90px] h-[50px] bg-purple-400 text-white font-semibold rounded-lg' onClick={todo}>COUNT</button>
            <input value={text} onChange={(e)=>settext(e.target.value)} className='bg-gray-400 rounded-lg'/>
         </div>
     );
