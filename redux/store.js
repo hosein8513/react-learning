d2ec551767983bfcf2e73f65d7e18d30f7baba21
hosein <kazmyh279@gmail.com>
2025-11-15 17:01:48 +0330

    using-redux-saga

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,22 @@
-import { createStore } from "redux";
-import { Reducer } from "./weather/reducer";
-import { applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
+import {
+    createStore
+} from "redux";
+import {
+    Reducer
+} from "./weather/reducer";
+import {
+    applyMiddleware
+} from "redux";
+import {
+    createSagaMiddleware
+} from "redux-saga"
+import {
+    weathersaga
+} from "./weather/weathersaga";
 
-export const Store = createStore(Reducer,applyMiddleware(thunk))
+const saga = createSagaMiddleware()
+
+export const Store = createStore(Reducer, applyMiddleware(saga))
+
+
+saga.run(weathersaga)
